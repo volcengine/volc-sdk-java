12ee7cd6075b0e9218ccb28b1c8d488e56d6e704
yefu <fuye@bytedance.com>
2020-12-17 15:04:14 +0800

    add imagex
    Change-Id: Ic92344ee41009a0de31562b510cdb1385eb4074b

---
+++ volc-sdk-java/src/main/java/com/volcengine/model/request/CommitImageUploadRequest.java
@@ -0,0 +1,22 @@
+package com.volcengine.model.request;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import com.volcengine.model.beans.Functions;
+import com.volcengine.model.beans.ImageOption;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class CommitImageUploadRequest {
+    @JSONField(name = "ServiceId")
+    String serviceId;
+    @JSONField(name = "SkipMeta")
+    Boolean skipMeta;
+    @JSONField(name = "SessionKey")
+    String sessionKey;
+    @JSONField(name = "Functions")
+    List<Functions> functions;
+    @JSONField(name = "OptionInfos")
+    List<ImageOption> optionInfos;
+}

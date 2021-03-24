7f39dda4c83fa8053788b1a037dfbbdb5ecf9ed2
王一帆 <wangyifan.co@bytedance.com>
2021-03-24 16:51:29 +0800

    business security risk detection

--- volc-sdk-java/src/main/java/com/volcengine/auth/impl/SignerV4Impl.java
+++ volc-sdk-java/src/main/java/com/volcengine/auth/impl/SignerV4Impl.java
@@ -6,9 +6,6 @@ import com.volcengine.helper.Const;
 import com.volcengine.helper.Utils;
 import com.volcengine.model.Credentials;
 import com.volcengine.service.SignableRequest;
-import com.volcengine.service.businessSecurity.BusinessSecurityConfig;
-import com.volcengine.service.businessSecurity.BusinessSecurityService;
-import com.volcengine.service.businessSecurity.impl.BusinessSecurityServiceImpl;
 import com.volcengine.util.NameValueComparator;
 import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.lang3.StringUtils;
@@ -42,7 +39,7 @@ public class SignerV4Impl implements ISignerV4 {
         H_INCLUDE.add("Content-Md5");
         H_INCLUDE.add("Host");
 
-
+    }
 
     @Override
     public void sign(SignableRequest request, Credentials credentials) throws Exception {

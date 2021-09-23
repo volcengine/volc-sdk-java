8908c13a0e855c01d8d8bbb8fb1f0933f654db2a
xionglangyu <xionglangyu@bytedance.com>
2021-09-23 17:29:23 +0800

    feat:article event notify

---
+++ volc-sdk-java/src/main/java/com/volcengine/model/response/ArticleEventNotifyResponse.java
@@ -0,0 +1,36 @@
+package com.volcengine.model.response;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class ArticleEventNotifyResponse {
+    @JSONField(name = "ResponseMetadata")
+    ResponseMetadata responseMetadata;
+    @JSONField(name = "Result")
+    ArticleEventNotifyResult result;
+
+    @Data
+    public static class ArticleEventNotifyResult {
+        @JSONField(name = "CursorTime")
+        Integer cursorTime;
+        @JSONField(name = "Items")
+        List<ArticleEvent> items;
+    }
+
+    @Data
+    public static class ArticleEvent {
+        @JSONField(name = "EventType")
+        String eventType;
+        @JSONField(name = "SubEventType")
+        String subEventType;
+        @JSONField(name = "EventTime")
+        Integer eventTime;
+        @JSONField(name = "ArticleId")
+        Long articleId;
+        @JSONField(name = "EventDetail")
+        String eventDetail;
+    }
+}

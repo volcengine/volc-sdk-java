1474cda2f85d474812cf8fb7459b4c758ea0304e
lizhanpeng <lizhanpeng@bytedance.com>
2021-10-22 15:11:24 +0800

    support image hiddenmark embec/extract

---
+++ volc-sdk-java/src/main/java/com/volcengine/model/response/ExtractImageHmResponse.java
@@ -0,0 +1,21 @@
+package com.volcengine.model.response;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import lombok.Data;
+
+@Data
+public class ExtractImageHmResponse {
+    @JSONField(name = "ResponseMetadata")
+    ResponseMetadata responseMetadata;
+    @JSONField(name = "Result")
+    ExtractImageHmResponse.ExtractImageHmResultBean result;
+
+    @Data
+    public static class ExtractImageHmResultBean {
+        @JSONField(name = "Info")
+        String info;
+        @JSONField(name = "StatusCode")
+        String statusCode;
+    }
+}
+

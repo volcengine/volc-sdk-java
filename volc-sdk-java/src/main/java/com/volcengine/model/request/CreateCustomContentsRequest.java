44779de0d008f38ca9bc66e5cadea19d77f57dd1
wangyifan.co <wangyifan.co@bytedance.com>
2022-02-10 11:36:50 +0800

    content securtiy

---
+++ volc-sdk-java/src/main/java/com/volcengine/model/request/CreateCustomContentsRequest.java
@@ -0,0 +1,16 @@
+package com.volcengine.model.request;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import lombok.Data;
+
+@Data
+public class CreateCustomContentsRequest {
+    @JSONField(name = "AppId")
+    Integer appId;
+    @JSONField(name = "Service")
+    String service;
+    @JSONField(name = "Decision")
+    String decision;
+    @JSONField(name = "Description")
+    String description;
+}
---
+++ volc-sdk-java/src/main/java/com/volcengine/service/contentSecurity/impl/ContentSecurityServiceImpl.java
@@ -0,0 +1,108 @@
+package com.volcengine.service.contentSecurity.impl;
+
+import com.alibaba.fastjson.JSON;
+import com.volcengine.error.SdkError;
+import com.volcengine.helper.Const;
+import com.volcengine.helper.Utils;
+import com.volcengine.model.request.*;
+import com.volcengine.model.response.*;
+import com.volcengine.service.BaseServiceImpl;
+import com.volcengine.service.contentSecurity.ContentSecurityConfig;
+import com.volcengine.service.contentSecurity.ContentSecurityService;
+
+import java.util.ArrayList;
+
+public class ContentSecurityServiceImpl extends BaseServiceImpl implements ContentSecurityService {
+    private ContentSecurityServiceImpl() {
+        super(ContentSecurityConfig.serviceInfo, ContentSecurityConfig.apiInfoList);
+    }
+
+    private static final ContentSecurityServiceImpl contentSecurityInstance = new ContentSecurityServiceImpl();
+
+    public static ContentSecurityService getInstance() {
+        return contentSecurityInstance;
+    }
+
+    @Override
+    public ImageRiskDetectionResponse ImageRiskDetection(RiskDetectionRequest riskDetectionRequest) throws Exception {
+        RawResponse response = json(Const.ImageContentRisk, new ArrayList<>(), JSON.toJSONString(riskDetectionRequest));
+        if (response.getCode() != SdkError.SUCCESS.getNumber()) {
+            throw response.getException();
+        }
+
+        return JSON.parseObject(response.getData(), ImageRiskDetectionResponse.class);
+    }
+
+    @Override
+    public AsyncRiskDetectionResponse AsyncImageRisk(RiskDetectionRequest riskDetectionRequest) throws Exception {
+        RawResponse response = json(Const.ImageContentRisk, new ArrayList<>(), JSON.toJSONString(riskDetectionRequest));
+        if (response.getCode() != SdkError.SUCCESS.getNumber()) {
+            throw response.getException();
+        }
+
+        return JSON.parseObject(response.getData(), AsyncRiskDetectionResponse.class);
+    }
+
+    @Override
+    public ImageRiskDetectionResponse ImageRiskResult(ImageRiskResultRequest riskDetectionRequest) throws Exception {
+        RawResponse response = query(Const.GetImageResult, Utils.mapToPairList(Utils.paramsToMap(riskDetectionRequest)));
+        if (response.getCode() != SdkError.SUCCESS.getNumber()) {
+            throw response.getException();
+        }
+
+        return JSON.parseObject(response.getData(), ImageRiskDetectionResponse.class);
+    }
+
+    @Override
+    public AsyncRiskDetectionResponse AsyncVideoRisk(RiskDetectionRequest riskDetectionRequest) throws Exception {
+        RawResponse response = json(Const.AsyncVideoRisk, new ArrayList<>(), JSON.toJSONString(riskDetectionRequest));
+        if (response.getCode() != SdkError.SUCCESS.getNumber()) {
+            throw response.getException();
+        }
+
+        return JSON.parseObject(response.getData(), AsyncRiskDetectionResponse.class);
+    }
+
+    @Override
+    public VideoRiskResultResponse VideoResult(ImageRiskResultRequest videoRiskResultRequest) throws Exception {
+        RawResponse response = query(Const.VideoResult, Utils.mapToPairList(Utils.paramsToMap(videoRiskResultRequest)));
+        if (response.getCode() != SdkError.SUCCESS.getNumber()) {
+            throw response.getException();
+        }
+        return JSON.parseObject(response.getData(), VideoRiskResultResponse.class);
+    }
+
+    @Override
+    public TextRiskResponse TextRisk(RiskDetectionRequest riskDetectionRequest) throws Exception {
+        RawResponse response = json(Const.TextRisk, new ArrayList<>(), JSON.toJSONString(riskDetectionRequest));
+        if (response.getCode() != SdkError.SUCCESS.getNumber()) {
+            throw response.getException();
+        }
+
+        String str = new String(response.getData());
+        System.out.println(str);
+
+        return JSON.parseObject(response.getData(), TextRiskResponse.class);
+    }
+
+    @Override
+    public AsyncRiskDetectionResponse CreateCustomContents(CreateCustomContentsRequest request) throws Exception {
+        RawResponse response = json(Const.CreateCustomContents, new ArrayList<>(), JSON.toJSONString(request));
+        if (response.getCode() != SdkError.SUCCESS.getNumber()) {
+            throw response.getException();
+        }
+
+        return JSON.parseObject(response.getData(), AsyncRiskDetectionResponse.class);
+    }
+
+    @Override
+    public AsyncRiskDetectionResponse UploadCustomContents(UploadCustomContentsRequest request) throws Exception {
+        RawResponse response = json(Const.UploadCustomContents, new ArrayList<>(), JSON.toJSONString(request));
+        if (response.getCode() != SdkError.SUCCESS.getNumber()) {
+            throw response.getException();
+        }
+
+        return JSON.parseObject(response.getData(), AsyncRiskDetectionResponse.class);
+    }
+
+}

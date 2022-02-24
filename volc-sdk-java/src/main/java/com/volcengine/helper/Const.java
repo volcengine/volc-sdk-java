46ef5f9e1973f2bd10f96713879c80b5863e2044
leiyixing <leiyixing@bytedance.com>
2022-02-24 16:57:53 +0800

    feat: merge for new apis

--- volc-sdk-java/src/main/java/com/volcengine/helper/Const.java
+++ volc-sdk-java/src/main/java/com/volcengine/helper/Const.java
@@ -9,10 +9,6 @@ public class Const {
     public static final String REGION_AP_SINGAPORE_1 = "ap-singapore-1";
     public static final String REGION_US_EASE_1 = "us-east-1";
 
-    public static final String INNER_REGION_CN_NORTH_1 = "cn-north-1-inner";
-    public static final String INNER_REGION_AP_SINGAPORT_1 = "ap-singapore-1-inner";
-    public static final String INNER_REGION_US_EAST_1 = "us-east-1-inner";
-
     public static final String TIME_FORMAT_V4 = "yyyyMMdd'T'HHmmss'Z'";
 
     public static final String CONNECTION_TIMEOUT = "ConnectionTimeout";
@@ -27,35 +23,34 @@ public class Const {
     public static final String Query = "Query";
     public static final String Form = "Form";
 
-    public static final String FORMAT_JPEG = "jpeg";
-    public static final String FORMAT_PNG = "png";
-    public static final String FORMAT_WEBP = "webp";
-    public static final String FORMAT_AWEBP = "awebp";
-    public static final String FORMAT_GIF = "gif";
-    public static final String FORMAT_HEIC = "heic";
-    public static final String FORMAT_ORIGINAL = "image";
-
-    public static final String HTTP = "http";
-    public static final String HTTPS = "https";
-
+    // vod
     public static final String VOD_TPL_OBJ = "tplv-vod-obj";
     public static final String VOD_TPL_NOOP = "tplv-vod-noop";
     public static final String VOD_TPL_RESIZE = "tplv-vod-rs";
     public static final String VOD_TPL_CENTER_CROP = "tplv-vod-cc";
     public static final String VOD_TPL_SMART_CROP = "tplv-vod-cs";
-    public static final String VOD_TPL_SIG = "tplv-bd-sig";
 
     public static final String SpaceName = "SpaceName";
 
-    // vod
+    public static final int MinChunkSize = 1024 * 1024 * 20;
+    public static final int LargeFileSize = 1024 * 1024 * 1024;
+
+
+    public static final String DSAHmacSha1 = "HMAC-SHA1";
+    public static final String DSAHmacSha256 = "HMAC-SHA256";
+    // vod-play
     public static final String GetPlayInfo = "GetPlayInfo";
-    public static final String StartWorkflow = "StartWorkflow";
+    public static final String GetPrivateDrmPlayAuth = "GetPrivateDrmPlayAuth";
+    public static final String GetHlsDecryptionKey = "GetHlsDecryptionKey";
+    public static final String GetPlayInfoWithLiveTimeShiftScene = "GetPlayInfoWithLiveTimeShiftScene";
+    // vod-upload
     public static final String UploadMediaByUrl = "UploadMediaByUrl";
+    public static final String QueryUploadTaskInfo = "QueryUploadTaskInfo";
     public static final String ApplyUploadInfo = "ApplyUploadInfo";
     public static final String CommitUploadInfo = "CommitUploadInfo";
-    public static final String QueryUploadTaskInfo = "QueryUploadTaskInfo";
-    public static final String UpdateMediaPublishStatus = "UpdateMediaPublishStatus";
+    // vod-media
     public static final String UpdateMediaInfo = "UpdateMediaInfo";
+    public static final String UpdateMediaPublishStatus = "UpdateMediaPublishStatus";
     public static final String GetMediaInfos = "GetMediaInfos";
     public static final String GetRecommendedPoster = "GetRecommendedPoster";
     public static final String DeleteMedia = "DeleteMedia";
@@ -64,9 +59,6 @@ public class Const {
     public static final String GetSubtitleInfoList = "GetSubtitleInfoList";
     public static final String UpdateSubtitleStatus = "UpdateSubtitleStatus";
     public static final String UpdateSubtitleInfo = "UpdateSubtitleInfo";
-    public static final String GetHlsDecryptionKey = "GetHlsDecryptionKey";
-    public static final String GetPrivateDrmPlayAuth = "GetPrivateDrmPlayAuth";
-    public static final String GetPlayInfoWithLiveTimeShiftScene = "GetPlayInfoWithLiveTimeShiftScene";
     public static final String GetAuditFramesForAudit = "GetAuditFramesForAudit";
     public static final String GetMLFramesForAudit = "GetMLFramesForAudit";
     public static final String GetBetterFramesForAudit = "GetBetterFramesForAudit";
@@ -77,45 +69,26 @@ public class Const {
     public static final String UpdateVideoClassification = "UpdateVideoClassification";
     public static final String DeleteVideoClassification = "DeleteVideoClassification";
     public static final String ListVideoClassifications = "ListVideoClassifications";
+    //vod-transcode
+    public static final String StartWorkflow = "StartWorkflow";
+    //vod-space
     public static final String CreateSpace = "CreateSpace";
     public static final String ListSpace = "ListSpace";
     public static final String GetSpaceDetail = "GetSpaceDetail";
     public static final String GetSpaceConfig = "GetSpaceConfig";
     public static final String UpdateSpace = "UpdateSpace";
     public static final String UpdateSpaceUploadConfig = "UpdateSpaceUploadConfig";
+    //vod-cdn
     public static final String ListDomain = "ListDomain";
-    public static final String CreateCdnRefreshTask = "CreateCdnRefreshTask";
-    public static final String CreateCdnPreloadTask = "CreateCdnPreloadTask";
+    public static final String CreateCdnRefreshTask = "CreateCDNRefreshTask";
+    public static final String CreateCdnPreloadTask = "CreateCDNPreloadTask";
+    //vod-callback
     public static final String AddCallbackSubscription = "AddCallbackSubscription";
     public static final String SetCallbackEvent = "SetCallbackEvent";
-
-
-    public static final int MinChunkSize = 1024 * 1024 * 20;
-    public static final int LargeFileSize = 1024 * 1024 * 1024;
-    public static final String FileTypeMedia = "media";
-    public static final String FileTypeImage = "image";
-    public static final String FileTypeObject = "object";
-    public static final String CategoryVideo = "video";
-    public static final String CategoryAudio = "audio";
-    public static final String CategoryImage = "image";
-    public static final String CategoryDynamicImg = "dynamic_img";
-    public static final String CategorySubtitle = "subtitle";
-    public static final String CategoryFont = "font";
-
-    public static final String DSAHmacSha1 = "HMAC-SHA1";
-    public static final String DSAHmacSha256 = "HMAC-SHA256";
 
     // iam
     public static final String CreateUser = "CreateUser";
     public static final String ListUsers = "ListUsers";
-
-    public static final String FILE_TYPE_VIDEO = "video";
-    public static final String FILE_TYPE_IMAGE = "image";
-    public static final String FILE_TYPE_OBJECT = "object";
-
-    // upload by url format type
-    public static final String UPLOAD_FORMAT_MP4 = "mp4";
-    public static final String UPLOAD_FORMAT_M3U8 = "m3u8";
 
     // ad blocker
     public static final String AdBlock = "AdBlock";

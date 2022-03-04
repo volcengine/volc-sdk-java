8fc490fc2595b73b7dbbfe0b76211963ad9e31d0
leiyixing <leiyixing@bytedance.com>
2022-03-04 15:58:45 +0800

    fix: fix callback config for test

--- volc-sdk-java/src/main/java/com/volcengine/service/vod/VodServiceConfig.java
+++ volc-sdk-java/src/main/java/com/volcengine/service/vod/VodServiceConfig.java
@@ -617,7 +617,7 @@ public class VodServiceConfig {
                             put(com.volcengine.helper.Const.Query, new ArrayList<NameValuePair>() {
                                 {
                                     add(new BasicNameValuePair("Action", Const.SetCallbackEvent));
-                                    add(new BasicNameValuePair("Version", "2021-12-01"));
+                                    add(new BasicNameValuePair("Version", "2021-01-01"));
                                 }
                             });
                         }

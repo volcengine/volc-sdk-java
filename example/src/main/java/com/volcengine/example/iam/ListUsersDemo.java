9e8627cb8db468ff595eafc1a2873f5e957ecbcd
baoyou <baoyou@bytedance.com>
2022-03-10 21:39:44 +0800

    add ListSnapshots api

--- example/src/main/java/com/volcengine/example/iam/ListUsersDemo.java
+++ example/src/main/java/com/volcengine/example/iam/ListUsersDemo.java
@@ -1,8 +1,8 @@
 package com.volcengine.example.iam;
 
 import com.alibaba.fastjson.JSON;
-import com.volcengine.model.request.ListUsersRequest;
-import com.volcengine.model.response.ListUsersResponse;
+//import com.volcengine.model.request.ListUsersRequest;
+//import com.volcengine.model.response.ListUsersResponse;
 import com.volcengine.service.iam.IIamService;
 import com.volcengine.service.iam.impl.IamServiceImpl;
 
@@ -15,14 +15,14 @@ public class ListUsersDemo {
         iamService.setAccessKey("ak");
         iamService.setSecretKey("sk");
         // list users
-        try {
-            ListUsersRequest listUsersRequest = new ListUsersRequest();
-            listUsersRequest.setLimit(3);
-
-            ListUsersResponse listUsersResponse = iamService.listUsers(listUsersRequest);
-            System.out.println(JSON.toJSONString(listUsersResponse));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//        try {
+//            ListUsersRequest listUsersRequest = new ListUsersRequest();
+//            listUsersRequest.setLimit(3);
+//
+//            ListUsersResponse listUsersResponse = iamService.listUsers(listUsersRequest);
+//            System.out.println(JSON.toJSONString(listUsersResponse));
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
     }
 }

9513fb9f051895adeabf01ee300efba0227bb7f0
zhangyuhang.astron <zhangyuhang.astron@bytedance.com>
2022-04-19 18:09:37 +0800

    feat: 内容定制推荐接口follow拆分videodetail

--- volc-sdk-java/src/main/java/com/volcengine/model/stream/GetListResponse.java
+++ volc-sdk-java/src/main/java/com/volcengine/model/stream/GetListResponse.java
@@ -78,7 +78,7 @@ public class GetListResponse {
         @JSONField(name = "ImageList")
         List<CommonPo.Image> imageList;
         @JSONField(name = "VideoDetail")
-        GetArticleResponse.VideoDetail videoDetail;
+        VideoDetail videoDetail;
     }
 
     @Data
@@ -87,5 +87,63 @@ public class GetListResponse {
         String name;
         @JSONField(name = "Id")
         String id;
+    }
+
+    @Data
+    public static class VideoDetail {
+        @JSONField(name = "Status")
+        int status;
+        @JSONField(name = "VideoDuration")
+        int videoDuration;
+        @JSONField(name = "PosterUrl")
+        String posterUrl;
+        @JSONField(name = "VideoId")
+        String videoId;
+        @JSONField(name = "VideoList")
+        List<VideoList> videoList;
+        @JSONField(name = "ArticleClass")
+        String articleClass;
+    }
+
+    @Data
+    public static class VideoList {
+        @JSONField(name = "MainUrl")
+        String mainUrl;
+        @JSONField(name = "MainHttpUrl")
+        String mainHttpUrl;
+        @JSONField(name = "BackupUrl1")
+        String backupUrl1;
+        @JSONField(name = "BackupHttpUrl")
+        String backupHttpUrl;
+        @JSONField(name = "UrlExpire")
+        long urlExpire;
+        @JSONField(name = "FileId")
+        String fileId;
+        @JSONField(name = "Bitrate")
+        int bitrate;
+        @JSONField(name = "CodecType")
+        String codecType;
+        @JSONField(name = "LogoType")
+        String logoType;
+        @JSONField(name = "Size")
+        int size;
+        @JSONField(name = "FPS")
+        int fps;
+        @JSONField(name = "Quality")
+        String quality;
+        @JSONField(name = "Encrypt")
+        boolean encrypt;
+        @JSONField(name = "FileHash")
+        String fileHash;
+        @JSONField(name = "Vheight")
+        int vHeight;
+        @JSONField(name = "Vwidth")
+        int vWidth;
+        @JSONField(name = "Vtype")
+        String vType;
+        @JSONField(name = "Definition")
+        String definition;
+        @JSONField(name = "Redirect")
+        boolean redirect;
     }
 }

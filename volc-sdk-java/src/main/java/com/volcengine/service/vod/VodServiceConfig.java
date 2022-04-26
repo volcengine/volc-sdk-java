72ebcdaac741d2f33676f2ad35b7ece5ff6bc2b8
xuruizhi <xuruizhi.01@bytedance.com>
2022-04-26 11:11:47 +0800

    add DescribeVodDomainBandwidthData config and demo

--- volc-sdk-java/src/main/java/com/volcengine/service/vod/VodServiceConfig.java
+++ volc-sdk-java/src/main/java/com/volcengine/service/vod/VodServiceConfig.java
@@ -621,6 +621,21 @@ public class VodServiceConfig {
                         }
                     }
             ));
+            put(Const.DescribeVodDomainBandwidthData, new ApiInfo(
+                    new HashMap<String, Object>() {
+                        {
+                            put(com.volcengine.helper.Const.Method, "GET");
+                            put(com.volcengine.helper.Const.Path, "/");
+                            put(com.volcengine.helper.Const.Query, new ArrayList<NameValuePair>() {
+                                {
+                                    add(new BasicNameValuePair("Action", Const.DescribeVodDomainBandwidthData));
+                                    add(new BasicNameValuePair("Version", "2020-08-01"));
+
+                                }
+                            });
+                        }
+                    }
+            ));
             // **********************************************************************
             // 回调管理
             // **********************************************************************

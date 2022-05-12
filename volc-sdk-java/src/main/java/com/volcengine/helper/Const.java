04b3a92e78f579d86abc5c8ec669ccc270151e7d
guozhifeng.felton <guozhifeng.felton@bytedance.com>
2022-05-12 21:41:20 +0800

    remove repeated const variables

--- volc-sdk-java/src/main/java/com/volcengine/helper/Const.java
+++ volc-sdk-java/src/main/java/com/volcengine/helper/Const.java
@@ -60,16 +60,6 @@ public class Const {
     public static final String TextRisk = "TextRisk";
     public static final String CreateCustomContents = "CreateCustomContents";
     public static final String UploadCustomContents = "UploadCustomContents";
-
-    // content security
-    public static final String ImageContentRisk = "ImageContentRisk";
-    public static final String AsyncImageRisk = "AsyncImageRisk";
-    public static final String VideoResult = "VideoResult";
-    public static final String GetImageResult = "GetImageResult";
-    public static final String AsyncVideoRisk = "AsyncVideoRisk";
-    public static final String TextRisk = "TextRisk";
-    public static final String CreateCustomContents = "CreateCustomContents";
-    public static final String UploadCustomContents = "UploadCustomContents";
     public static final String GetAudioResult = "GetAudioResult";
     public static final String AsyncAudioRisk = "AsyncAudioRisk";
     public static final String GetVideoLiveResult = "GetVideoLiveResult";

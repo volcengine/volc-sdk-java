8bb26d95c066c0e64f0ebe65d902feb4e18f1493
林周玉 <linzhouyu.dev@bytedance.com>
2022-06-02 15:10:21 +0800

    添加 DescribeIPListInfo

--- example/src/main/java/com/volcengine/example/cdn/DescribeIPListInfo.java
+++ example/src/main/java/com/volcengine/example/cdn/DescribeIPListInfo.java
@@ -5,15 +5,15 @@ import com.volcengine.model.beans.CDN;
 import com.volcengine.service.cdn.CDNService;
 import com.volcengine.service.cdn.impl.CDNServiceImpl;
 
-public class DescribeIPInfo {
+public class DescribeIPListInfo {
     public static void main(String[] args) {
         CDNService service = CDNServiceImpl.getInstance();
         service.setAccessKey(Utils.ak);
         service.setSecretKey(Utils.sk);
         try {
-            CDN.DescribeIPInfoRequest req = new CDN.DescribeIPInfoRequest();
-            req.setIP("1.1.1.1");
-            CDN.DescribeIPInfoResponse resp = service.describeIPInfo(req);
+            CDN.DescribeIPListInfoRequest req = new CDN.DescribeIPListInfoRequest();
+            req.setIpList("1.1.1.1,2.2.2.2");
+            CDN.DescribeIPListInfoResponse resp = service.describeIPListInfo(req);
             System.out.println(JSON.toJSONString(resp));
         } catch (Exception e) {
             e.printStackTrace();

1200373c5cfb84b66e3dd11ffd5fdb532cfc6402
林周玉 <linzhouyu.dev@bytedance.com>
2022-06-06 20:28:27 +0800

    delete test ak/sk

--- example/src/main/java/com/volcengine/example/cdn/Utils.java
+++ example/src/main/java/com/volcengine/example/cdn/Utils.java
@@ -5,8 +5,8 @@ import java.util.Date;
 public class Utils {
     public static long startTime = new Date().getTime()/1000 - 86400;
     public static long endTime = startTime + 86400;
-    public static String ak = "***REMOVED***";
-    public static String sk = "***REMOVED***";
+    public static String ak = "";
+    public static String sk = "";
     public static String exampleHost = "www.example.com";
     public static String exampleUrl1 = "http://www.example.com/1.txt";
     public static String exampleUrl2 = "http://www.example.com/2.txt";

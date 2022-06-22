8a5970891338394efff0511ab5735cd2a609c8fe
leiyixing <leiyixing@bytedance.com>
2022-06-22 23:02:36 +0800

    fix: update

--- example/src/main/java/com/volcengine/example/vod/cdn/VodListCdnPvDataDemo.java
+++ example/src/main/java/com/volcengine/example/vod/cdn/VodListCdnPvDataDemo.java
@@ -2,10 +2,10 @@ package com.volcengine.example.vod.cdn;
 
 import com.volcengine.service.vod.IVodService;
 import com.volcengine.service.vod.impl.VodServiceImpl;
-import com.volcengine.service.vod.model.request.VodListCdnStatusDataRequest;
+import com.volcengine.service.vod.model.request.VodListCdnPvDataRequest;
 import com.volcengine.service.vod.model.response.VodCdnStatisticsCommonResponse;
 
-public class VodListCdnStatusDataDemo {
+public class VodListCdnPvDataDemo {
     public static void main(String[] args) throws Exception {
         IVodService vodService = VodServiceImpl.getInstance();
         // call below method if you dont set ak and sk in ～/.vcloud/config
@@ -13,14 +13,14 @@ public class VodListCdnStatusDataDemo {
         vodService.setSecretKey("your sk");
 
         try {
-            VodListCdnStatusDataRequest.Builder input = VodListCdnStatusDataRequest.newBuilder();
+            VodListCdnPvDataRequest.Builder input = VodListCdnPvDataRequest.newBuilder();
             input.setDomains("your domains");
             input.setStartTimestamp(0);
             input.setEndTimestamp(0);
             input.setDataType("your dataType");
             input.setInterval("your interval");
             input.setMetric("your metric");
-            VodCdnStatisticsCommonResponse resp = vodService.listCdnStatusData(input.build());
+            VodCdnStatisticsCommonResponse resp = vodService.listCdnPvData(input.build());
             System.out.println(resp);
         } catch (Exception e) {
             e.printStackTrace();

863871ffa4455f301c52a5eb239519c1851a0376
zhangshiqi.17 <zhangshiqi.17@bytedance.com>
2022-06-30 17:32:23 +0800

    rawResponse新增header
    Change-Id: I92199ba59f8f896355fe37b81c667407348751d0

--- volc-sdk-java/src/main/java/com/volcengine/model/response/RawResponse.java
+++ volc-sdk-java/src/main/java/com/volcengine/model/response/RawResponse.java
@@ -1,6 +1,11 @@
 package com.volcengine.model.response;
 
 import lombok.Data;
+import org.apache.http.Header;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Data
 public class RawResponse {
@@ -8,7 +13,7 @@ public class RawResponse {
     private byte[] data;
     private int code;
     private Exception exception;
-
+    private Map<String,String> headers;
 
     public RawResponse(byte[] data, int code, Exception e) {
         this.data = data;

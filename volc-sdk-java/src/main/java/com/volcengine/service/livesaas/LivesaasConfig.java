27911c4f3817a7d648bbb4423427ec1ef5996916
陈嘉瑞 <chenjiarui.jory@bytedance.com>
2022-12-12 05:25:27 +0000

    feat: increase timeout

--- volc-sdk-java/src/main/java/com/volcengine/service/livesaas/LivesaasConfig.java
+++ volc-sdk-java/src/main/java/com/volcengine/service/livesaas/LivesaasConfig.java
@@ -20,7 +20,7 @@ public class LivesaasConfig {
                     new HashMap<String, Object>() {
                         {
                             put(Const.CONNECTION_TIMEOUT, 5000);
-                            put(Const.SOCKET_TIMEOUT, 5000);
+                            put(Const.SOCKET_TIMEOUT, 10000);
                             put(Const.Scheme, "https");
                             put(Const.Host, "livesaas.volcengineapi.com");
                             put(Const.Header, new ArrayList<Header>() {

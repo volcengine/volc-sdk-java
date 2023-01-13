8317d299f43cc43cbb25ddf945ee9beb5b28a5ef
雷懿行 <leiyixing@bytedance.com>
2023-01-13 10:34:35 +0000

    Vod

--- volc-sdk-java/src/main/java/com/volcengine/service/vod/VodServiceConfig.java
+++ volc-sdk-java/src/main/java/com/volcengine/service/vod/VodServiceConfig.java
@@ -483,6 +483,20 @@ public class VodServiceConfig {
                         }
                     }
             ));
+            put(Const.ExtractMediaMetaTask, new ApiInfo(
+                    new HashMap<String, Object>() {
+                        {
+                            put(com.volcengine.helper.Const.Method, "GET");
+                            put(com.volcengine.helper.Const.Path, "/");
+                            put(com.volcengine.helper.Const.Query, new ArrayList<NameValuePair>() {
+                                {
+                                    add(new BasicNameValuePair("Action", Const.ExtractMediaMetaTask));
+                                    add(new BasicNameValuePair("Version", "2022-01-01"));
+                                }
+                            });
+                        }
+                    }
+            ));
 
             // **********************************************************************
             // 转码

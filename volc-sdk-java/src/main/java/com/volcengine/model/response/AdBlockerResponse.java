c9730ddcdb88e260da4237ed5ea6f5c349c3cf96
liyan.90210 <liyan.90210@bytedance.com>
2024-01-25 11:26:28 +0000

    feat auto update sdk

--- volc-sdk-java/src/main/java/com/volcengine/model/response/AdBlockerResponse.java
+++ volc-sdk-java/src/main/java/com/volcengine/model/response/AdBlockerResponse.java
@@ -32,5 +32,27 @@ public class AdBlockerResponse {
         private List<Integer> tags;
         @JSONField(name = "Detail")
         private String detail;
+        @JSONField(name = "Contexts")
+        private List<Context> contexts;
+    }
+
+    @Data
+    public static class Context {
+        @JSONField(name = "MatchedWords")
+        private List<String> matchedWords;
+        @JSONField(name = "LibName")
+        private String libName;
+        @JSONField(name = "Description")
+        private String description;
+        @JSONField(name = "Positions")
+        private List<Position> positions;
+    }
+
+    @Data
+    public static class Position {
+        @JSONField(name = "StartPos")
+        private Integer startPos;
+        @JSONField(name = "EndPos")
+        private Integer endPos;
     }
 }

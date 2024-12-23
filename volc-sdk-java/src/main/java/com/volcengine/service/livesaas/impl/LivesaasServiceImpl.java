4ad3579d1b61a0a9e495cf84c311a2a7ee653888
liyan.90210 <liyan.90210@bytedance.com>
2024-12-23 13:16:40 +0000

    feat auto update sdk

--- volc-sdk-java/src/main/java/com/volcengine/service/livesaas/impl/LivesaasServiceImpl.java
+++ volc-sdk-java/src/main/java/com/volcengine/service/livesaas/impl/LivesaasServiceImpl.java
@@ -3764,6 +3764,36 @@ public class LivesaasServiceImpl extends BaseServiceImpl implements LivesaasServ
     }
 
     @Override
+    public GetActivityPosterResponse getActivityPoster(GetActivityPosterRequest getActivityPosterRequest) throws Exception {
+        RawResponse response = query(Const.GetActivityPoster, Utils.paramsToPair(getActivityPosterRequest));
+        if (response.getCode() != SdkError.SUCCESS.getNumber()) {
+            throw response.getException();
+        }
+        GetActivityPosterResponse res = JSON.parseObject(response.getData(), GetActivityPosterResponse.class);
+        if (res.getResponseMetadata().getError() != null) {
+            ResponseMetadata meta = res.getResponseMetadata();
+            throw new Exception(meta.getRequestId() + "error: " + meta.getError().getMessage());
+        }
+        res.getResponseMetadata().setService("livesaas");
+        return res;
+    }
+
+    @Override
+    public UpdateActivityPosterResponse updateActivityPoster(UpdateActivityPosterRequest updateActivityPosterRequest) throws Exception {
+        RawResponse response = json(Const.UpdateActivityPoster, new ArrayList<>(),JSON.toJSONString(updateActivityPosterRequest));
+        if (response.getCode() != SdkError.SUCCESS.getNumber()) {
+            throw response.getException();
+        }
+        UpdateActivityPosterResponse res = JSON.parseObject(response.getData(), UpdateActivityPosterResponse.class);
+        if (res.getResponseMetadata().getError() != null) {
+            ResponseMetadata meta = res.getResponseMetadata();
+            throw new Exception(meta.getRequestId() + "error: " + meta.getError().getMessage());
+        }
+        res.getResponseMetadata().setService("livesaas");
+        return res;
+    }
+
+    @Override
     public GetLinkUserAmountResponse getLinkUserAmount(GetLinkUserAmountRequest getLinkUserAmountRequest) throws Exception {
         RawResponse response = query(Const.GetLinkUserAmount, Utils.paramsToPair(getLinkUserAmountRequest));
         if (response.getCode() != SdkError.SUCCESS.getNumber()) {
